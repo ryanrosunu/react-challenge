05a893db4a8d8a2eda866c43277b767161dd2b53
ryanrosunu <ryanrosu2027@u.northwestern.edu>
2026-04-08 14:33:09 -0500

    Completed fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,26 @@
 // import viteLogo from '/vite.svg'
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+import { useJsonQuery } from './utilities/fetch';
 
 const App = () => {
-  return (
-    <>
-      <div className="bg-[#bf77f6] min-h-screen">
-        <header className="flex flex-col items-center text-white">
-          <Banner title={schedule.title} />
-        </header>
-        <CourseList courses={schedule.courses} />
-      </div>
-    </>
-    
-  );
-};
+  const [json, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
 
-export default App
+  if (error) return <h1>Error loading schedule data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading schedule data...</h1>;
+  if (!json) return <h1>No schedule data found</h1>;
+
+  // Type assertion for fetched schedule
+  const schedule = json as { title: string; courses: Record<string, { term: string; number: string; meets: string; title: string }> };
+
+  return (
+    <div className="bg-[#bf77f6] min-h-screen">
+      <header className="flex flex-col items-center text-white">
+        <Banner title={schedule.title} />
+      </header>
+      <CourseList courses={schedule.courses} />
+    </div>
+  );
+}
+
+export default App;
